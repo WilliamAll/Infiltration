7075a54ced6f934a75902d08ac27c3d21aa80c08
WilliamAll <wil53@hotmail.fr>
2023-03-02 09:36:43 +0100

    Player Rotation Update

--- Assets/Script/Player_Look.cs
+++ Assets/Script/Player_Look.cs
@@ -18,7 +18,7 @@ public class Player_Look : MonoBehaviour
 
     private void Update()
     {
-        //RayCastDebug();
+        RayCastDebug();
     }
 
     private void OnEnable()
@@ -43,10 +43,15 @@ public class Player_Look : MonoBehaviour
     public void OnLook(InputValue value) //InputAction.CallbackContext context
     {
         Vector2 mouseDelta = value.Get<Vector2>() * sensitivity * Time.deltaTime; //may should be fixed update
+        transform.rotation = Quaternion.Euler(0, transform.eulerAngles.y + mouseDelta.x, 0f); //that line allow to rotate on "y" only
         //Debug.Log("Value of MouseDelta" + mouseDelta);
-        float currentXRotation = transform.eulerAngles.x;
-        float newXRotation = currentXRotation - mouseDelta.y;
-        transform.rotation = Quaternion.Euler(newXRotation, transform.eulerAngles.y + mouseDelta.x, 0f);
+        //float currentXRotation = transform.eulerAngles.x; //if look to floor is say 90 if look front is 0 or 360
+        //float newXRotation = currentXRotation - mouseDelta.y; //that line set the vertical orientation
+        //transform.rotation = Quaternion.Euler(newXRotation, transform.eulerAngles.y + mouseDelta.x, 0f); //that line allow vertical orientation
+
+
+
+
 
         //RayCastDebug();
     }
