157496164358a885840ab1173f84c8e296f56b78
WilliamAll <wil53@hotmail.fr>
2023-03-06 20:38:38 +0100

    Stash Update
    I just press it

---
+++ Assets/Script/Idle_Tree.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Idle_Tree : StateMachineBehaviour
+{
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            animator.SetBool("isSneak", true);
+        }
+        animator.SetBool("isSprint", false);
+
+    }
+}
