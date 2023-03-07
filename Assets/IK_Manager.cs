f4dec580ece3c25b0590d48238d105408b195470
WilliamAll <wil53@hotmail.fr>
2023-03-07 12:27:07 +0100

    IK added but inactive, Animation go Humanoid

--- Assets/IK_Manager.cs
+++ Assets/IK_Manager.cs
@@ -14,17 +14,32 @@ public class IK_Manager : MonoBehaviour
 
     private void Awake()
     {
-        TryGetComponent<Animator>(out _animator);
+
     }
 
-    private void OnAnimatorIK(int layerIndex)
+    private void Start()
     {
+        TryGetComponent<Animator>(out _animator);
+        if(_animator != null)
+        {
+            Debug.Log("Get Animator at IK");
+        }
+        else
+        {
+            Debug.LogError("No Animator at IK");
+        }
+    }
+
+    private void OnAnimatorIK()
+    {
+        Debug.Log("OnAnimatorIK");
         if(_animator)
         {
             if(_activeIK)
             {
                 if(_targetIK != null)
                 {
+                    Debug.Log("isIk");
                     _animator.SetLookAtWeight(1);
                     _animator.SetLookAtPosition(_targetIK.position);
                 }
